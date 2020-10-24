f9532173809d8f1b75df858f5862418ef2c060ce
Steven Butter <stevenbutter.451@gmail.com>
2020-10-24 21:59:34 +0100

    Refactor

--- RadioSixPlaylistConverter.py
+++ RadioSixPlaylistConverter.py
@@ -6,16 +6,32 @@ def ConvertToPlaylist(websitePlaylist):
 
 
 def _ConvertToPlaylistItem(websitePlaylistItem):
+    splitItem = _SplitItem(websitePlaylistItem)
+    artists = _SplitArtists(splitItem[0])
+    trackName = splitItem[1]
+
+    return PlaylistEntry(
+        _GetMainArtist(artists),
+        trackName,
+        featuredArtists=_GetFeatArtists(artists))
+
+
+def _SplitItem(item):
     HyphenSep = ' - '
     DashSep = ' – '
+
+    return item.split(HyphenSep if HyphenSep in item else DashSep, 2)
+
+
+def _SplitArtists(item):
     FeatArtistSep = ' ft. '
 
-    separated = websitePlaylistItem.split(
-        HyphenSep if HyphenSep in websitePlaylistItem else DashSep, 2)
-    artists = separated[0].split(FeatArtistSep, 2)
-    trackName = separated[1]
+    return item.split(FeatArtistSep, 2)
 
-    mainArtist = artists[0]
-    featArtist = artists[1] if len(artists) == 2 else None
 
-    return PlaylistEntry(mainArtist, trackName, featuredArtists=featArtist)
+def _GetMainArtist(artists):
+    return artists[0]
+
+
+def _GetFeatArtists(artists):
+    return artists[1] if len(artists) == 2 else None
