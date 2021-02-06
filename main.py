120bdce719df3a0fdb8d9cdc2852e336a1cdad4a
Steven Butter <stevenbutter.451@gmail.com>
2021-02-06 15:22:11 +0000

    Add date to playlist

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import RadioSixPlaylist
 import spotipy
 import Spotify
+from datetime import datetime
+
+
+def _GetDate():
+    return datetime.today().strftime('%Y-%m-%d')
+
 
 if __name__ == "__main__":
     playlist = RadioSixPlaylist.GetPlaylist()
 
     spotifyClient = spotipy.Spotify(
         auth_manager=spotipy.SpotifyOAuth(
-            redirect_uri='https://example.com', 
+            redirect_uri='https://example.com',
             scope='playlist-modify-private'))
 
     spotify = Spotify.Spotify(spotifyClient)
-    clientPlaylist = spotify.CreatePlaylist("r6")
+    clientPlaylist = spotify.CreatePlaylist('{} - {}'.format('r6', _GetDate()))
     spotify.AddToPlaylist(clientPlaylist, playlist)
-    
